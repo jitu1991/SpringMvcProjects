11e592500c96aa4af8c0eee1e1c84693e84b340e
Jitender Kumar <jitender.kumar@irissoftware.com>
2019-03-26 21:22:22 +0530

    First Mvc Run

---
+++ SpringMvc/WebContent/WEB-INF/HelloPage.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h1>Spring Mvc</h1>
+<h2>${welcomeMessage}</h2>
+</body>
+</html>
---
+++ SpringMvc/src/com/test/mvc/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.test.mvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		ModelAndView model = new ModelAndView("HelloPage");
+		model.addObject("welcomeMessage", "Hi User");
+		return model;
+	}
+
+}
