6239c8628d9b98d97ecc7ab8238dc91041427cd6
Jitender Kumar <jitender.kumar@irissoftware.com>
2019-03-30 10:58:43 +0530

    postHandle and afterCompletion method implemention

--- SpringMvc/src/com/test/mvc/controller/DayBasedAccessInterceptor.java
+++ SpringMvc/src/com/test/mvc/controller/DayBasedAccessInterceptor.java
@@ -6,17 +6,30 @@ import java.util.Calendar;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class DayBasedAccessInterceptor extends HandlerInterceptorAdapter {
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws IOException {
 		Calendar calendar = Calendar.getInstance();
 		int day = calendar.get(calendar.DAY_OF_WEEK);
-		if(day == 6) {
+		if (day == 6) {
 			response.getWriter().write("app is closed on friday");
 			return false;
 		}
 		return true;
 	}
+
+	@Override
+	public void postHandle(HttpServletRequest req, HttpServletResponse res, Object handler, ModelAndView modelAndview) {
+		System.out.println("postHandler method called: after handler method preHandle() executes");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest req, HttpServletResponse res, Object handler, Exception ex) throws Exception {
+		System.out.println("afterCompletion method called: after view resolver executes");
+	}
+
 }
